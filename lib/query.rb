8c434aed5807815c1081b32f68fbd0b912ae95ea
Apprentice <apprentice@devbootcamp.com>
2012-10-29 16:51:29 -0700

    Create query class with initialize and search

---
+++ lib/query.rb
@@ -0,0 +1,24 @@
+# require_relative './search_result'
+require 'nokogiri'
+require 'open-uri'
+
+class NotCraigsListError < StandardError
+  def message
+    "Not the right url"
+  end
+end
+
+class Query
+  def initialize(url)
+    raise NotCraigsListError unless url =~ /craigslist/
+    @url = url
+  end
+
+  def search
+    SearchResult.from_nokogiri(url_to_nokogiri_document, Time.now)
+  end
+
+  def url_to_nokogiri_document
+    Nokogiri::HTML(open(@url))
+  end
+end
