597a792846f266f0a997e9afb622ea3b76ed8802
Richard Van Breemen + Guillaume Galuz <apprentice+rvb+gg@devbootcamp.com>
2012-10-30 14:47:37 -0700

    implemented the #save method for search result objects (lib)

--- lib/search_result.rb
+++ lib/search_result.rb
@@ -1,22 +1,38 @@
 require 'open-uri'
 require 'nokogiri'
 require 'date'
+require 'posting'
+require 'sqlite3'
 
 class SearchResult
-  attr_reader :postings, :time
+  attr_reader :postings
+  attr_accessor :searched_at
 
-  def initialize
-    @postings = []
-    @time
+  def initialize(postings = [])
+    @postings = postings
+    @searched_at
   end
 
-  def self.from_nokogiri(nokogiri, time)
+  def self.from_nokogiri(nokogiri, searched_at)
     search_result = self.new
-    search_result.time = time
+    search_result.searched_at = searched_at
     nokogiri.css('.row').each do |result|
       search_result.postings << Posting.new(parse(result))
     end
     search_result
+  end
+
+  def save(query_id, db_name)
+    # begin
+      @db = open_db(db_name)
+      create_search_results_table
+      insert_self_into_search_results_table(query_id)
+      id = retrieve_id_from_db
+      @postings.each { |posting| posting.save(id, db_name) }
+      true
+    # rescue
+    #   false
+    # end
   end
 
   private
@@ -28,6 +44,24 @@ class SearchResult
       category = row.at_css(".itemcg").text.strip
       url = row.at_css("a").attributes["href"].value
       {:posted_at => posted_at, :title => title, :price => price, :location => location, :category => category, :url => url }
+    end
+
+    def open_db(db_name)
+      SQLite3::Database.open(db_name)
+    end
+
+    def create_search_results_table
+      @db.execute "CREATE TABLE IF NOT EXISTS search_results(Id INTEGER PRIMARY KEY AUTOINCREMENT,
+              searched_at DATETIME, query_id INT)"
+    end
+
+    def insert_self_into_search_results_table(query_id)
+      @db.execute "INSERT INTO search_results(searched_at, query_id)
+                   VALUES (\"#{self.searched_at}\", \"#{query_id}\")"
+    end
+
+    def retrieve_id_from_db
+      (@db.execute "SELECT Id FROM search_results ORDER BY Id desc LIMIT 1")[0][0]
     end
 
 end
